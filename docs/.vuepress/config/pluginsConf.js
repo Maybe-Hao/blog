b2719390f78d994703774f6d278e10f48ef153e6
Maybe-Hao <1348114176@qq.com>
2022-03-05 00:51:23 +0800

    demo

---
+++ docs/.vuepress/config/pluginsConf.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+    'meting':
+    {
+       // metingApi: {},
+       meting: {
+         auto: 'https://music.163.com/#/playlist?id=2230996180'
+       },
+       aplayer: {
+        fixed: true,
+        lrcType: 3
+       },
+       mobile: {
+        cover: true,
+        lrc: true
+       },
+    },
+    'go-top': {},
+    "cursor-effects": {},
+    "@vuepress-reco/vuepress-plugin-kan-ban-niang":
+    {
+      theme: [
+        'haru1', 'haru2'
+      ],
+      clean: true,
+      messages: {
+        welcome: '欢迎来到可爱的小李浩的站点嗷~~',
+        home: '心里的花，我想要带你回家。',
+        theme: '好吧，希望你能喜欢我的小李浩哟。',
+        close: '你知道我喜欢吃什么吗？痴痴地望着你。'
+      }
+    }
+}
